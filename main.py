91dff1f2bc45b97bc1ac7c86c5976fed52765b03
timop <polzert@bzz.ch>
2024-09-02 14:34:28 +0200

    done

--- main.py
+++ main.py
@@ -1,17 +1,21 @@
 """Kochbuch-App"""
 import json
 
+
 def adjust_recipe(my_recipe, my_num_people):
     factor = my_num_people / my_recipe['servings']
-    adjusted_ingredients = {ingredient: int(quantity * factor) for ingredient, quantity in my_recipe['ingredients'].items()}
+    adjusted_ingredients = {ingredient: int(quantity * factor) for ingredient, quantity in my_recipe['ingredients']
+    .items()}
     return {
         "title": my_recipe["title"],
         "ingredients": adjusted_ingredients,
         "servings": my_num_people
     }
 
+
 def load_recipe(my_json_string):
     return json.loads(my_json_string)
+
 
 if __name__ == '__main__':
     recipe_json = '{"title": "Spaghetti Bolognese", "ingredients": {"Spaghetti": 400, "Tomato Sauce": 300, "Minced Meat": 500}, "servings": 4}'
