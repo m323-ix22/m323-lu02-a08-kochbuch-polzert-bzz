06faec2bc3cbefbb7b922594463cb6d805fefd72
timop <polzert@bzz.ch>
2024-09-02 14:32:16 +0200

    done

--- main.py
+++ main.py
@@ -1,6 +1,21 @@
-# Dein Code kommt hier hin
+"""Kochbuch-App"""
+import json
+
+def adjust_recipe(my_recipe, my_num_people):
+    factor = my_num_people / my_recipe['servings']
+    adjusted_ingredients = {ingredient: int(quantity * factor) for ingredient, quantity in my_recipe['ingredients'].items()}
+    return {
+        "title": my_recipe["title"],
+        "ingredients": adjusted_ingredients,
+        "servings": my_num_people
+    }
+
+def load_recipe(my_json_string):
+    return json.loads(my_json_string)
 
 if __name__ == '__main__':
-    # Beispiel für die Datenstruktur eines Rezepts
     recipe_json = '{"title": "Spaghetti Bolognese", "ingredients": {"Spaghetti": 400, "Tomato Sauce": 300, "Minced Meat": 500}, "servings": 4}'
-    # Dein Code kommt hier hin
+    recipe = load_recipe(recipe_json)
+    print(recipe)
+    adjusted_recipe = adjust_recipe(recipe, 2)
+    print(adjusted_recipe)
